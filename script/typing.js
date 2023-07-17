a47919de6e5c9eab4e8e2ec2ceb21d971159689d
mingjung414 <137235890+mingjung414@users.noreply.github.com>
2023-07-17 17:41:34 +0900

    Add files via upload

---
+++ script/typing.js
@@ -0,0 +1,39 @@
+const $text = document.querySelector(".text");
+
+// 글자 모음
+const letters = [
+  "SILLY",
+];
+
+// 글자 입력 속도
+const speed = 500;
+let i = 0;
+
+// 타이핑 효과
+const typing = async () => {  
+  const letter = letters[i].split("");
+  
+  while (letter.length) {
+    await wait(speed);
+    $text.innerHTML += letter.shift(); 
+  }
+  
+  
+  // 잠시 대기
+  await wait(800);
+  
+
+
+  
+  // 다음 순서의 글자로 지정, 타이핑 함수 다시 실행
+  i = !letters[i+1] ? 0 : i + 1;
+  typing();
+}
+
+// 딜레이 기능 ( 마이크로초 )
+function wait(ms) {
+  return new Promise(res => setTimeout(res, ms))
+}
+
+// 초기 실행
+setTimeout(typing, 1500);
